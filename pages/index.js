bc2881627e3587eb882ee69dbce145e516088e36
ericNull <eric.e.nicolas@gmail.com>
2023-02-27 16:08:35 +0900

    emit to all front-end done

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,28 @@
 import { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
-let socket = io("http://localhost:4000",{ transports: ["websocket"] });
+let socketIo = io("http://localhost:4000",{ transports: ["websocket"] });
 export default function Home() {
   
  const [message, setMessage]=useState("");
+ const [messages, setMessages]=useState([]);
 
   function sendMessage(){
          console.log("entering");
-         socket.emit("message", {payload:message});
+         socketIo.emit("message", {message});
+         const node = document.getElementsByClassName('text');
+         node[0].value = '';
          setMessage(" ");
          
   }
+  socketIo.on("sentback", (payload)=>{setMessages([...messages, payload])})
   return (
   <div>
-  <input type="text" name="message" placeholder="message to send" onChange={(e)=>setMessage(e.target.value)}/>
+  <input className ='text'type="text" name="message" placeholder="message to send" onChange={(e)=>setMessage(e.target.value)}/>
   <button onClick={sendMessage}>send message</button>
-  <div>{message}</div>
+  {
+  messages.map((mes,key)=><div>{mes}</div>
+)}
   </div>
   )
 }
