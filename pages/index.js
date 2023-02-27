bee83101a7dc216bd5e0297c055552e743a9cc83
ericNull <eric.e.nicolas@gmail.com>
2023-02-27 17:32:59 +0900

    messaged are saved to default firestore

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
 import { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+
+var config = {
+  apiKey: "AIzaSyB9014wHsIBzRhLDvoSbwq3KsO4x8xOGtg",
+  authDomain: "solo-chat-e3be7.firebaseapp.com",
+  projectId: "solo-chat-e3be7",
+  storageBucket: "solo-chat-e3be7.appspot.com",
+  messagingSenderId: "66142476835",
+  appId: "1:66142476835:web:cd4f1c23b2d1faaf08ef96"
+};
+firebase.initializeApp(config);
+const db = firebase.firestore();
+
 
 let socketIo = io("http://localhost:4000",{ transports: ["websocket"] });
 export default function Home() {
@@ -10,6 +25,10 @@ export default function Home() {
   function sendMessage(){
          console.log("entering");
          socketIo.emit("message", {message});
+         db.collection("main_chat").add({
+          message: message
+        //  timestamp: db.firestore.FieldValue.serverTimestamp()
+        })
          const node = document.getElementsByClassName('text');
          node[0].value = '';
          setMessage(" ");
